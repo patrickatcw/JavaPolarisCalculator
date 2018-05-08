58b6b0930d990664bb882f8c687c5fea09dec288
patrickatcw <pj8392@yahoo.com>
2018-05-07 21:51:55 -0400

    more notes added

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -2,6 +2,31 @@ package com.me;
 
 //make simple calculator that shows both user input and
 //result
+//one class, operations? maybe
+//allow for negative numbers, add, subtract, multiply, divide
+//how to store numbers, with pressing an operator being the end of input numbers
+//      1 + 2 + 45 + = , beginning of operation
+//store by string input;
+//how do numbers get saved to have operations performed
+//coupling operators
+//would a list be created? by the whole equation, then sending that list to
+//operator class to iterate through methods
+//for each number in listnumber
+/*
+var input;
+var x;
+var operator;
+
+input = 123;
+operator = +;
+x = input;
+input = 456;
+x = x + input;
+operator = -;
+input 123;
+operator ;= =;
+x = x - input
+ */
 public class Main {
 
     public static void main(String[] args) {
